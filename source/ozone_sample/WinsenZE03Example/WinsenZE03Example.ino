74be2aea7fd9dd593c194931bc3d8946ef4d4ec8
Nhan Nguyen <ngdangnhan@yahoo.com>
2019-09-18 09:23:54 +0630

    Ozone sample with ESP8266

---
+++ source/ozone_sample/WinsenZE03Example/WinsenZE03Example.ino
@@ -0,0 +1,25 @@
+/*
+  WinsenZE03.h - This library allows you to set and read the ZE03 Winsen Sensor module.
+	More information: https://github.com/fega/winsen-ze03-arduino-library
+  Created by Fabian Gutierrez <fega.hg@gmail.com>, March 17, 2017.
+  MIT.
+*/
+
+#include <WinsenZE03.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial gtSerial(D5, D8); // Arduino RX, Arduino TX
+WinsenZE03 sensor;
+
+
+void setup() {
+  gtSerial.begin(115200);
+  Serial.begin(115200);
+  sensor.begin(&gtSerial, O3);
+	sensor.setAs(QA);
+}
+void loop() {
+ float ppm = sensor.readManual();
+ Serial.print("OZONE VALUE:");
+ Serial.println(ppm);
+}
