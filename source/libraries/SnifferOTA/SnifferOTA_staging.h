3e55f7c110378f5a512c361b9b8b7ce95ff99adf
doanthuyan <doanthuyan@gmail.com>
2020-08-19 15:06:28 +0700

    Fix fingerprint Update display name Bump version

--- source/libraries/SnifferOTA/SnifferOTA_staging.h
+++ source/libraries/SnifferOTA/SnifferOTA_staging.h
@@ -5,7 +5,6 @@
 
 #define SERVER_ADDRESS_STAGING "raw.githubusercontent.com"
 #define RELEASE_URL_STAGING "/aavn/AirSniffer/master/staging/package.json"
-#define SERVER_FINGER_STAGING "CC:AA:48:48:66:46:0E:91:53:2C:9C:7C:23:2A:B1:74:4D:29:9D:33"
 #define DOWNLOAD_URL_STAGING "https://raw.githubusercontent.com/aavn/AirSniffer/master/staging/"
 
 	
